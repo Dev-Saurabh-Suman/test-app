6d8f94de6f5d8bbe71dab88624b24e578dfb904a
Saurabh Suman <srbhsuman289@gmail.com>
2022-09-19 14:32:09 +0530

    setup

---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	<h1>Welcome To The Users Home Page</h1><br><br>
+	<h3>For Admin Login Click here<a href="">Login</a></h3><br>
+	<h3>For Employee Login Click here<a href="">Login</a></h3><br>
+
+</body>
+</html>
